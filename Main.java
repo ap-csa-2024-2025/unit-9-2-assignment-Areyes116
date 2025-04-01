cb0719abe1f3f759d1583f6a38721d8c4d6f1eb3
Areyes116 <adrian.reyes2006@icloud.com>
2025-04-01 12:16:20 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public class Main
-{
-  public static void main(String[] args)
-  {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    // Uncomment to test the Car classes
-    // CarTester.run();
+        // Uncomment to test the Car classes
+        CarTester.run();
 
-    // Uncomment to test the Company classes
-    // CompanyTester.run();
-  }
+        // Uncomment to test the Company classes
+        CompanyTester.run();
+
+        scanner.close();
+    }
 }
