8ff1629302800b01d9452b08e39c355067ffb403
Areyes116 <adrian.reyes2006@icloud.com>
2025-04-01 12:15:31 -0700

    Update CompanyTester.java

--- CompanyTester.java
+++ CompanyTester.java
@@ -1,7 +1,15 @@
-public class CompanyTester
-{
-	public static void run()
-	{
+public class CompanyTester {
+    public static void run() {
+        // Create a Company object
+        Company generalCompany = new Company("TechCorp", "123 Tech St", "Innovation City", "CA");
 
-	}
+        // Print the Company object
+        System.out.println(generalCompany);
+
+        // Create an OnlineCompany object
+        OnlineCompany onlineCompany = new OnlineCompany("CodeHS", "www.codehs.com");
+
+        // Print the OnlineCompany object
+        System.out.println(onlineCompany);
+    }
 }
